c46da53ea084e492bc2deaac52d33c810d165a8d
Steffen Barszus <steffenbpunkt@googlemail.com>
2011-07-17 06:36:19 +0000

    update to latest git, fix debug spew, enable all LIRC, update mantis ir and lock patch.

--- make-dkms.sh
+++ make-dkms.sh
@@ -68,7 +68,7 @@ cd ..
 echo "s2-liplianin: Update ended. Now: $VERSION"
 }
 
-KERNEL=2.6.38-8-generic
+KERNEL=2.6.38-10-generic
 RELEASE=natty
 if [ -z "$KERNEL" ]; then
     if [ ! -z "$2" ]; then
